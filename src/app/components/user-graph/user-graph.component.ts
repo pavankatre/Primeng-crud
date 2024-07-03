9138534ffe72ab1749a2a523c164c585cdb175b7
Pavan Katre <pavankatre@gmail.com>
2024-07-03 23:22:27 +0530

    add scss for the graph

--- src/app/components/user-graph/user-graph.component.ts
+++ src/app/components/user-graph/user-graph.component.ts
@@ -42,8 +42,8 @@ export class UserGraphComponent {
         {
           label: 'Sales',
           data: [this.age18to30, this.age31to50, this.age51to70],
-          backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
-          borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
+          backgroundColor: ['rgba(255, 159, 64, 0.2)',  'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
+          borderColor: ['rgb(255, 159, 64)',  'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
           borderWidth: 1
         }
       ]
